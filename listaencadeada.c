a6a7966ed38a8a0c1f57ac2b77bef1ecfa50b719
Luiz Felipe Silvano <luizfsilvano@gmail.com>
2024-03-06 01:48:24 -0300

    primeiro commit

---
+++ listaencadeada.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct registro
+{
+    int valor;
+    struct registro *proximo;
+} registro;
+
+typedef struct lista
+{
+    int qtd;
+    registro *inicio;
+} lista;
+
+mostrarLista(lista *l, int qtd)
+{
+    printf("\n\nQuantidade de itens na lista: %d\n\n", qtd);
+    registro *r = l->inicio;
+    while (r != NULL)
+    {
+        printf("valor: %d\n\n", r->valor);
+        r = r->proximo;
+    }
+}
+
+lista *criarLista()
+{
+    lista *l = (lista *)malloc(sizeof(lista));
+    l->qtd = 0;
+    l->inicio = NULL;
+    return l;
+}
+
+registro *criarRegistro()
+{
+    registro *r = (registro *)malloc(sizeof(registro));
+    r->valor = 0;
+    r->proximo = NULL;
+    return r;
+}
+
+void inserirInicio(lista *l, int valor)
+{
+    registro *r = criarRegistro();
+    r->proximo = l->inicio;
+    l->inicio = r;
+    r->valor = valor;
+    l->qtd++;
+}
+
+void inserirFim(lista *l, int valor)
+{
+    registro *novo = criarRegistro();
+    registro *aux;
+    novo->valor = valor;
+
+    if (l->inicio == NULL)
+    {
+        l->inicio = novo;
+    }
+    else
+    {
+        aux = l->inicio;
+        while (aux->proximo != NULL)
+        {
+            aux = aux->proximo;
+        }
+        aux->proximo = novo;
+    }
+
+    l->qtd++;
+}
+
+void remover(lista *l, int valor)
+{
+    registro *aux = NULL, *ex = l->inicio;
+
+    if (l->inicio == NULL) {
+        printf("\n\nNão existem registros nessa lista!\n\n");
+        return;
+    }
+    while (ex != NULL && ex->valor != valor)
+    {
+        aux = ex;
+        ex = ex->proximo;
+    }
+
+    if (ex == NULL)
+    {
+        printf("\nValor não encontrado!\n");
+        return;
+    }
+
+    if (aux == NULL)
+    {
+        l->inicio = ex->proximo;
+    }
+    else
+    {
+        aux->proximo = ex->proximo;
+    }
+
+    free(ex);
+    l->qtd--;
+}
+
+void menu()
+{
+    int opcao;
+    int rodando = 1;
+    lista *l = criarLista();
+    while (rodando == 1)
+    {
+        printf("1 - Inserir no inicio\n");
+        printf("2 - Inserir no final\n");
+        printf("4 - Mostrar lista\n");
+        printf("5 - Remover\n");
+        printf("6 - Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            printf("\nInsira o valor do registro: ");
+            int valor;
+            scanf("%d", &valor);
+            inserirInicio(l, valor);
+            break;
+        case 2:
+            printf("\nInsira o valor do registro: ");
+            scanf("%d", &valor);
+            inserirFim(l, valor);
+            break;
+        case 3:
+            // inserirMeio();
+            break;
+        case 4:
+            mostrarLista(l, l->qtd);
+            break;
+        case 5:
+            printf("\nDigite o valor que quer remover: ");
+            scanf("%d", &valor);
+            remover(l, valor);
+            break;
+        case 6:
+            rodando = 0;
+            break;
+        }
+    }
+}
+
+int main()
+{
+
+    menu();
+
+    return 0;
+}
